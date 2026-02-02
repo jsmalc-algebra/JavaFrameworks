6da9c1491caa6e61e289738ebcd546ebfcc0ee8f
jsmalc-algebra <jsmalc2@algebra.hr>
2026-02-02 16:50:29 +0100

    Initial commit

---
+++ src/main/java/hr/algebra/jsmalc/online_retial_project/OnlineRetialProjectApplication.java
@@ -0,0 +1,13 @@
+package hr.algebra.jsmalc.online_retial_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineRetialProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineRetialProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hr/algebra/jsmalc/online_retial_project/OnlineRetialProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.algebra.jsmalc.online_retial_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineRetialProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
